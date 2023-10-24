e76e62e731201dd9391e8342dda5558d002947fe
Akbhardwaj5959 <141863378+Akbhardwaj5959@users.noreply.github.com>
2023-10-24 22:08:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function threeSum(arr, target) {
 // write your code here
+	let n=arr.length;
+	let intialdeff= Infinity , ans=-1;
+	for(let i=0; i<=n-3; i++){
+		for(let j=i+1; j<=n-2; j++){
+			for(let k= j+1; k<=n-1; k++){
+				let sum = arr[i]+arr[j]=arr[k];
+				let deff = Math.abs(sum-target);
+				if(intialdeff >diff){
+					ans=sum;
+				}
+				
+			}
+		}
+	}
+	console.log(ans);
   
 }
 
